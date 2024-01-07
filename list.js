f015f5ae711d77bf46d28a030454664a6b680c28
60936 <studio34pl@gmail.com>
2024-01-07 08:55:01 +0800

    r

---
+++ list.js
@@ -0,0 +1,69 @@
+function displayAnswer(elementId) { //about button
+  if (elementId === 'values') {
+  alert(`
+  R-eliability, 
+  E-xemplary in codes, 
+  S-ervice to society, 
+  C-oncern for standards, 
+  U-nited in our mission, 
+  E-njoyment in what we do
+  .(KeengWatanabe)`);
+} else if (elementId === 'mission') {
+  alert(`to learn the steps to creating a shopping cart without all the theory`);
+}
+else if (elementId === 'vision') {
+  alert(`to be a leading guru of tech development and education`);
+}
+else if (elementId === 'contents') {
+  window.open(href="/contents.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 'disclaimer') {
+  alert(`This content is created for trainee and trained lifesaver's references only.  Context of saving methodology is subject to relevant skills, experiences and expertise interpretation, content here are by no means comprehensive enough to be used alone. The proper way to be an effective lifesaver is to undertake a course of training provided by a licensed trainer and certified by a recognised lifesaving authority. (chatbot developer)`);
+} //topics button
+else if (elementId === 'css01') {
+  window.open(href="videos/css 01-bootstrapIcons.mp4",'chat AI','resizable,height=300,width=500');
+
+}
+else if (elementId === 'css02') {
+  window.open(href="videos/css 02-class,ID 25.12.2023_08.36.25_REC.mp4",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's3Tow') {
+  window.open(href="videos/s3Tow.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's4Resuscitation') {
+  window.open(href="videos/s4Resuscitation.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's4WaterEAR') {
+  window.open(href="videos/s4WaterEAR.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's5EmergencyCare') {
+  window.open(href="videos/s5EmergencyCare.html",'chat AI','resizable,height=300,width=500');
+}
+
+
+else if (elementId === 's6CardiovascularSys') {
+  window.open(href="videos/s6CardiovascularSys.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's6RespiratoryIntro') {
+  window.open(href="videos/s6RespiratorySys.html",'chat AI','resizable,height=300,width=500');
+}
+
+else if (elementId === 's6Spinal') {
+  window.open(href="videos/s6Spinal.html",'chat AI','resizable,height=300,width=500');
+
+}
+else if (elementId === 's7Legal') {
+  window.open(href="videos/s7Legality.html",'chat AI','resizable,height=300,width=500');
+
+}
+else if (elementId === 's8Rescue') {
+  window.open(href="videos/s8Rescue.html",'chat AI','resizable,height=300,width=500');
+}
+else if (elementId === 's8Resuscitation') {
+  window.open(href="videos/s8Resuscitation.html",'chat AI','resizable,height=300,width=500');
+}    
+
+
+
+
+}
