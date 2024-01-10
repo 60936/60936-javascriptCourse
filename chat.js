f7b1051e271c7879c39debed3556cfe907e1eaac
60936 <studio34pl@gmail.com>
2024-01-10 13:53:02 +0800

    r

--- chat.js
+++ chat.js
@@ -11,14 +11,15 @@ function question() {
 else if(inputText.toLowerCase().indexOf("thank you")>-1){
 			document.getElementById("answer").innerHTML="You are welcome.不用客气。";}
 
-else if(inputText.toLowerCase().indexOf("add user")>-1){document.getElementById("answer").innerHTML=`git config --global user.email "name@email.com<br>git config --global user.name "webdev"<br>git commit -m "version 1"`;}
+else if(inputText.toLowerCase().indexOf("add user")>-1&&inputText.toLowerCase().indexOf("github")>-1){document.getElementById("answer").innerHTML=`git config --global user.email "name@email.com<br>git config --global user.name "webdev"<br>git commit -m "version 1"`;}
+else if(inputText.toLowerCase().indexOf("add repo")>-1&&inputText.toLowerCase().indexOf("github")>-1){document.getElementById("answer").innerHTML=`remote name, 'origin' <br> remote URL, https://github.com/KeenGWatanabe/repo.git`;}
 
+else if(inputText.toLowerCase().indexOf("add repo")>-1&&inputText.toLowerCase().indexOf("github")>-1&&inputText.toLowerCase().indexOf("eg")>-1){document.getElementById("answer").innerHTML=`git remote add origin https://github.com/KeengWatanabe/repo.git <br>git remote -v<br>will verify new remote as below; <br>origin https://github.com/KeengWatanable/repo.git (fetch)<br>origin https://github.com/KeengWatanable/repo.git (push)`;}
 
-//"landings" keyword prompt* 
-else if(inputText.toLowerCase().indexOf("landing")>-1){
-	document.getElementById("answer").innerHTML=" try keyword 'landing' with: types, walk out, drag, shoulder carry, pick a back carry, stirrup, assisted lift, straight arm lift, use (lift and lower) with this methods ";
-}
-	 else {
+else if(inputText.toLowerCase().indexOf("pull repo")>-1&&inputText.toLowerCase().indexOf("github")>-1&&inputText.toLowerCase().indexOf("eg")>-1){document.getElementById("answer").innerHTML=`git clone https://github.com/KeengWatanabe/repo.git <br>clone diff name:<br>git clone https://github.com/KeengWatanable/repo.git repo1.git`;}
+
+else if(inputText.toLowerCase().indexOf("push repo")>-1&&inputText.toLowerCase().indexOf("github")>-1&&inputText.toLowerCase().indexOf("eg")>-1){document.getElementById("answer").innerHTML=`git push https://github.com/KeengWatanabe/repo.git `;}
+else {
 		answerElement.innerHTML = `I do not understand your input '${inputText}', perhaps you could click our "topics" button above or check your spelling.<br>Or email us what was your question for us to update an appropriate response.<br><a href="mailto:tonboswimmers@gmail.com?&subject=Lifeguard%20ChatBot%20Enquiry&body=Thank%20you%20for%20your%20email%20to%20Tonboswimmers", target="_blank">email</a>`;
 		      
 	}
